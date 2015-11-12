bd3842e74d7f2923e7c0991f6699686924c1bdce
pocketberserker <pocketberserker@gmail.com>
2015-11-13 01:27:52 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FSharp.Automaton
+
+[![License][license-image]][license-url]
+
+FSharp.Automaton is a port of [automaton](https://github.com/evancz/automaton).
+
+[license-url]: https://github.com/pocketberserker/FSharp.Automaton/blob/master/LICENSE
+[license-image]: https://img.shields.io/github/license/pocketberserker/FSharp.Automaton.svg
---
+++ src/FSharp.Automaton/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharp.Automaton.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("FSharp.Automaton")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyProduct("FSharp.Automaton")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("896b2e49-99ea-4210-a778-ac1c2f2e9873")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do
+  ()
