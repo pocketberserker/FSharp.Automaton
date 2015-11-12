0ae31e9dccd31a35b586fed589cbcb8b10ab43a6
pocketberserker <pocketberserker@gmail.com>
2015-11-13 03:41:05 +0900

    use FAKE

--- src/FSharp.Automaton/AssemblyInfo.fs
+++ src/FSharp.Automaton/AssemblyInfo.fs
@@ -1,16 +1,15 @@
-﻿namespace FSharp.Automaton.AssemblyInfo
-
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyTitle("FSharp.Automaton")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyProduct("FSharp.Automaton")>]
-[<assembly: ComVisible(false)>]
-[<assembly: Guid("896b2e49-99ea-4210-a778-ac1c2f2e9873")>]
-[<assembly: AssemblyVersion("0.1.0.0")>]
-[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitleAttribute("FSharp.Automaton")>]
+[<assembly: AssemblyDescriptionAttribute("Arrowized FRP for F#.")>]
+[<assembly: GuidAttribute("896b2e49-99ea-4210-a778-ac1c2f2e9873")>]
+[<assembly: AssemblyProductAttribute("FSharp.Automaton")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
 
-do
-  ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
