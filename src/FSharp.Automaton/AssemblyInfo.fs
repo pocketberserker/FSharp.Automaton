48b97f768127f998708ddde91f487dd216b6769b
pocketberserker <pocketberserker@gmail.com>
2015-11-14 16:07:20 +0900

    Bump version to 0.2.0

--- src/FSharp.Automaton/AssemblyInfo.fs
+++ src/FSharp.Automaton/AssemblyInfo.fs
@@ -6,10 +6,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("Arrowized FRP for F#.")>]
 [<assembly: GuidAttribute("896b2e49-99ea-4210-a778-ac1c2f2e9873")>]
 [<assembly: AssemblyProductAttribute("FSharp.Automaton")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] Version = "0.2.0"
